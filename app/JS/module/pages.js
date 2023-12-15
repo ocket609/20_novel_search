513043450f65381f255a86b24a1489319e77dc8c
阿博 <snsn15965412@gmail.com>
2023-12-16 03:24:26 +0800

    json-server部屬網址追加

--- app/JS/module/pages.js
+++ app/JS/module/pages.js
@@ -1,119 +1,99 @@
-
-
 const booksGet = "http://localhost:3000/books";
 const commentGet = "http://localhost:3000/books?_expand=comment";
 
 let data = [];
 
-
 //接收bookId
 
 const value = window.location.search;
-const newvalue = value.split('=')[1];
+const newvalue = value.split("=")[1];
 //帶入bookId
 
 PagebookApi(newvalue);
 
 //PagesBooksApi
 
-const pagesbody = document.querySelector('body');
+const pagesbody = document.querySelector("body");
 
-function PagebookApi(p){
-    axios.get(`http://localhost:3000/books/${p}?_embed=comments`)
-    
-      .then(function(response){
-        data = response.data;
-        pageRender(data);
-        PageCommentRender(data);
-        
-      })
-      .catch(function(error){
-          document.querySelector('.bookSection');
-          pagesbody.innerHTML = ``;
-          console.log(error);
-          window.setTimeout("alert('無效連結，將導向回首頁')",50);
-          setTimeout("location.href='/'",500);
-        
-      })
+function PagebookApi(p) {
+  axios
+    .get(`https://demo-9j6o.onrender.com/books/${p}?_embed=comments`)
+
+    .then(function (response) {
+      data = response.data;
+      pageRender(data);
+      PageCommentRender(data);
+    })
+    .catch(function (error) {
+      document.querySelector(".bookSection");
+      pagesbody.innerHTML = ``;
+      console.log(error);
+      window.setTimeout("alert('無效連結，將導向回首頁')", 50);
+      setTimeout("location.href='/'", 500);
+    });
 }
 
-
 //PagesRender
-const book = document.querySelector('.bookSection');
-const bookImg = document.querySelector('.bookImg');
-const bookName = document.querySelector('.bookName2');
-const bookInfoLeft = document.querySelector('.bookInfoLeft');
-const bookInfoRight = document.querySelector('.bookInfoRight');
-const bookDescrt = document.querySelector('.bookDescrt');
-const bookHeart = document.querySelector('.bookHeart');
+const book = document.querySelector(".bookSection");
+const bookImg = document.querySelector(".bookImg");
+const bookName = document.querySelector(".bookName2");
+const bookInfoLeft = document.querySelector(".bookInfoLeft");
+const bookInfoRight = document.querySelector(".bookInfoRight");
+const bookDescrt = document.querySelector(".bookDescrt");
+const bookHeart = document.querySelector(".bookHeart");
 
 function pageRender(bookData) {
-
-
-            if(bookData == 0){
-              albeer('無此頁面')
-            }else{
-
-            bookImg.innerHTML = `<div class="bookImgDiv">
+  if (bookData == 0) {
+    albeer("無此頁面");
+  } else {
+    bookImg.innerHTML = `<div class="bookImgDiv">
             <img src=${bookData.img} class="bookImg" alt="我真的太難了" style="min-width:250px;">
            </div> `;
 
-            bookName.innerHTML = ` 
+    bookName.innerHTML = ` 
               <h2>${bookData.bookName}</h2>
               <h4>${bookData.author}</h4>
                    `;
-                   
 
-          if(bookData.tags.length === 2){
-              
-            bookInfoLeft.innerHTML = `
+    if (bookData.tags.length === 2) {
+      bookInfoLeft.innerHTML = `
             <p >連載狀態 : ${bookData.SerializationStatus}</p>
             <p>集數 : ${bookData.Episode}</p>
             <p>類別 : <span class="bg-success  p-1 text-white rounded-3">${bookData.tags[0]}</span><span class="bg-success ms-3 p-1 text-white rounded-3">${bookData.tags[1]}</span></p>
             <p>語言 : ${bookData.language}</p>`;
-
-          }else{
-            bookInfoLeft.innerHTML = `
+    } else {
+      bookInfoLeft.innerHTML = `
             <p >連載狀態 : ${bookData.SerializationStatus}</p>
             <p>集數 : ${bookData.Episode}</p>
             <p>類別 : <span class="bg-success  p-2 text-white rounded-3">${bookData.tags}</span>
             <p>語言 : ${bookData.language}</p>`;
-          }
+    }
 
-           
-
-           bookInfoRight.innerHTML = `
+    bookInfoRight.innerHTML = `
            <div class="col-6"><p><a class="outsideSiteLink text-dark-50" href="${bookData.link}" >思兔閱讀</a></p></div>
            <div class="col-6"><p><a class="outsideSiteLink text-dark-50" href="https://sto520.com/" >YY書網</a></p></div>
            <div class="col-6"><p><a class="outsideSiteLink text-dark-50" href="https://sto520.com/" >ZZ書網</a></p></div>
            <div class="col-6"><p><a class="outsideSiteLink text-dark-50" href="https://sto520.com/" >XX書網</a></p></div>`;
 
-           bookDescrt.innerHTML = `
+    bookDescrt.innerHTML = `
            <p class="p-4 lh-lg">${bookData.description}</p>
            `;
-          }
-          
-          
+  }
 }
-
 
 //PagesCommentRender
 
-const commentListArea = document.querySelector('.mySwiper');
-const commentArea = document.querySelector('.commentArea');
-
+const commentListArea = document.querySelector(".mySwiper");
+const commentArea = document.querySelector(".commentArea");
 
 function PageCommentRender(commentAllData) {
+  let str = "";
 
-      let str = '';
-
-      if(commentAllData.comments.length === 0){
-        commentArea.innerHTML = `<h2 class="text-center mb-5">無任何留言</h2>`
-      }else{
-
-        commentAllData.comments.forEach((item,index) => {
-
-          str +=  `
+  if (commentAllData.comments.length === 0) {
+    commentArea.innerHTML = `<h2 class="text-center mb-5">無任何留言</h2>`;
+  } else {
+    commentAllData.comments.forEach((item, index) => {
+      str += `
           <swiper-slide class="card bg-orange-300 position-relative mt-5 swiper-slide-active " 
           role="group" style="width: 365.333px;height:auto; margin-right: 10px;>
           <div  class="">
@@ -131,142 +111,124 @@ function PageCommentRender(commentAllData) {
            </div>  
        </div>       
     </swiper-slide>
-          `
-          GoodcommentData(item.id,index);
-      })
-    }
-    commentListArea.innerHTML = str;
-    GoodcommentCheckForDisplay();
-
+          `;
+      GoodcommentData(item.id, index);
+    });
+  }
+  commentListArea.innerHTML = str;
+  GoodcommentCheckForDisplay();
 }
-
 
 //move to Search.html
 
-const pageArea = document.querySelector('.searchDiv');
-const pageSearch = document.querySelector('.search')
+const pageArea = document.querySelector(".searchDiv");
+const pageSearch = document.querySelector(".search");
 
-pageArea.addEventListener('click',pageSearchToSearchPage);
+pageArea.addEventListener("click", pageSearchToSearchPage);
 
 function pageSearchToSearchPage() {
-   console.log(pageSearch.value);
+  console.log(pageSearch.value);
 
-     if(pageSearch.value === undefined || pageSearch.value === ''){
-      return;
-     }else{
-      let result = pageSearch.value;
-      pageSearch.value = '';
-      console.log(result);
-      window.open(encodeURI(`http://127.0.0.1:5501/app/search.html?result=${result}`));
-
-     }
-
+  if (pageSearch.value === undefined || pageSearch.value === "") {
+    return;
+  } else {
+    let result = pageSearch.value;
+    pageSearch.value = "";
+    console.log(result);
+    window.open(
+      encodeURI(
+        `https://ocket609.github.io/20_novel_search/app/search.html?result=${result}`
+      )
+    );
+  }
 }
-
 
 //聆聽收藏書籍
 
 const newvalueNum = Number(newvalue);
-let bookLocal = localStorage.getItem('bookId');
+let bookLocal = localStorage.getItem("bookId");
 
-bookHeart.addEventListener('click',goodBookListener);
+bookHeart.addEventListener("click", goodBookListener);
 
-function goodBookListener(){
-  
-   let bookLSdata = JSON.parse(bookLocal);
-   if(bookLSdata=== null){
-    localStorage.setItem('bookId',JSON.stringify([]));
+function goodBookListener() {
+  let bookLSdata = JSON.parse(bookLocal);
+  if (bookLSdata === null) {
+    localStorage.setItem("bookId", JSON.stringify([]));
     return;
-  }else if(bookLSdata.includes(newvalueNum)){
+  } else if (bookLSdata.includes(newvalueNum)) {
     let index = bookLSdata.indexOf(newvalueNum);
-    bookLSdata.splice(index,1);
-    localStorage.setItem('bookId',JSON.stringify(bookLSdata));
+    bookLSdata.splice(index, 1);
+    localStorage.setItem("bookId", JSON.stringify(bookLSdata));
     location.reload();
-
-  }else{
+  } else {
     bookLSdata.push(newvalueNum);
-    localStorage.setItem('bookId',JSON.stringify(bookLSdata));
+    localStorage.setItem("bookId", JSON.stringify(bookLSdata));
     location.reload();
   }
-
-
 }
-
 
 //判斷是否收藏書籍
 
-window.addEventListener('load',goodBookCheckForDisplay);
+window.addEventListener("load", goodBookCheckForDisplay);
 
-function goodBookCheckForDisplay(){
-
+function goodBookCheckForDisplay() {
   let bookLSdata = JSON.parse(bookLocal);
 
-  if (bookLSdata.includes(newvalueNum)){
-    bookHeart.setAttribute('src', './assets/images/heart-full.svg');
-  }else{
-    bookHeart.setAttribute('src', './assets/images/heart.svg');
+  if (bookLSdata.includes(newvalueNum)) {
+    bookHeart.setAttribute("src", "./assets/images/heart-full.svg");
+  } else {
+    bookHeart.setAttribute("src", "./assets/images/heart.svg");
   }
 }
-
-
-
 
 //判斷是否按讚留言
 
 let goodComment = [];
 
-let heartLocal = localStorage.getItem('heartId');
+let heartLocal = localStorage.getItem("heartId");
 
-function GoodcommentData(id,index){
-
+function GoodcommentData(id, index) {
   let test = JSON.parse(heartLocal);
-  if(test === null){
-    localStorage.setItem('heartId',JSON.stringify([]));
+  if (test === null) {
+    localStorage.setItem("heartId", JSON.stringify([]));
     return;
-  }else if(test.indexOf(id) !== -1){
-
+  } else if (test.indexOf(id) !== -1) {
     goodComment.push(index);
-
- }
+  }
 }
 
 //渲染留言按讚
 
-function GoodcommentCheckForDisplay(){
-  
-  const commentHeart = document.querySelectorAll('.commentheart'); 
+function GoodcommentCheckForDisplay() {
+  const commentHeart = document.querySelectorAll(".commentheart");
   goodComment.forEach((item) => {
-       
-    commentHeart[item].setAttribute('src', './assets/images/thumb_up_alt.svg')
-  })
+    commentHeart[item].setAttribute("src", "./assets/images/thumb_up_alt.svg");
+  });
   goodComment = [];
 }
 
-
 //聆聽按讚
 
-const swiperTest = document.querySelector('.mySwiper');
+const swiperTest = document.querySelector(".mySwiper");
 
-swiperTest.addEventListener('click',Goodcommentlistener);
+swiperTest.addEventListener("click", Goodcommentlistener);
 
-function Goodcommentlistener(e){
-
+function Goodcommentlistener(e) {
   let heartLocalData = JSON.parse(heartLocal);
   let value = e.target.dataset.heartid;
   let numValue = Number(value);
 
-  if(value === null || value === NaN || value === undefined){
+  if (value === null || value === NaN || value === undefined) {
     return;
-  }else{
-    if(heartLocalData.includes(numValue)){
-      let index =  heartLocalData.indexOf(numValue);
-      heartLocalData.splice(index,1);
-      localStorage.setItem('heartId',JSON.stringify(heartLocalData));
+  } else {
+    if (heartLocalData.includes(numValue)) {
+      let index = heartLocalData.indexOf(numValue);
+      heartLocalData.splice(index, 1);
+      localStorage.setItem("heartId", JSON.stringify(heartLocalData));
       location.reload();
-  
-    }else{
+    } else {
       heartLocalData.push(numValue);
-      localStorage.setItem('heartId',JSON.stringify(heartLocalData));
+      localStorage.setItem("heartId", JSON.stringify(heartLocalData));
       location.reload();
     }
   }
--- app/JS/module/search.js
+++ app/JS/module/search.js
@@ -1,48 +1,48 @@
-
-const booksGet = "http://localhost:3000/books";
+const booksGet = "https://demo-9j6o.onrender.com/books";
 let data = [];
-
 
 //預載入
 
 searchPageApi();
 
 //Search Area
-const searchPageSearch = document.querySelector('.search');
-const searchPageSearchImg = document.querySelector('.searchImg');
+const searchPageSearch = document.querySelector(".search");
+const searchPageSearchImg = document.querySelector(".searchImg");
 
 //Select Area
-const selectArea = document.querySelector('.selectArea');
-const searchPageSelectCategory = document.querySelector('.selectCategory');
-const searchPageSelectStar = document.querySelector('.selectStar');
+const selectArea = document.querySelector(".selectArea");
+const searchPageSelectCategory = document.querySelector(".selectCategory");
+const searchPageSelectStar = document.querySelector(".selectStar");
 
 //addEventListener
-searchPageSearchImg.addEventListener('click', function() {searchRender(searchPageSearch.value,data)});
-selectArea.addEventListener('change',selectRender);
+searchPageSearchImg.addEventListener("click", function () {
+  searchRender(searchPageSearch.value, data);
+});
+selectArea.addEventListener("change", selectRender);
 
 //searchPageApi
 
- function searchPageApi(){
-  axios.get(booksGet)
-  
-    .then(function(response){
-        data = response.data;
-        searchResult(data);
+function searchPageApi() {
+  axios
+    .get(booksGet)
+
+    .then(function (response) {
+      data = response.data;
+      searchResult(data);
     })
-    .catch(function(error){
-        console.log(error);
-    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-
 //searchPageRender
-const bookListArea = document.querySelector('.bookList');
+const bookListArea = document.querySelector(".bookList");
 
 function searchPageRender(bookAllData) {
-      let str = '';
+  let str = "";
 
-      bookAllData.forEach((item,index) => {
-            str +=  `<div class="col p-0 position-relative m-3"  style="width: 15rem;height: 32rem; " >
+  bookAllData.forEach((item, index) => {
+    str += `<div class="col p-0 position-relative m-3"  style="width: 15rem;height: 32rem; " >
             <img src=${item.img} class="card-img-top" alt="..." data-id=${item.id} style="height: 360px;">
             <div class="d-flex position-absolute top-50 end-0 mt-5  align-items-center ">
               <div><span class="p-1" role="button"><img class="pagesIcon" src="./assets/images/star2.svg" alt="star"></span></div>
@@ -55,187 +55,163 @@ function searchPageRender(bookAllData) {
                 <div class="col-xl-12 p-1 startag "><img class="starImg" src="./assets/images/star.svg" alt="star">${item.Star}</div>
                </div> 
             </div>
-        </div> `
-        GoodBookData(item.id,index);
-      })
+        </div> `;
+    GoodBookData(item.id, index);
+  });
 
-     bookListArea.innerHTML = str;
-     GoodBookCheckForDisplay();
-
+  bookListArea.innerHTML = str;
+  GoodBookCheckForDisplay();
 }
 
 //search render
 
-function searchlistener(){
+function searchlistener() {}
 
-}
-
- function searchRender(r,data){
-    location.search = `result=${r}`;
-    let newData  = data.filter((item) => item.bookName.includes(r));
-    if(newData.length === 0){
-      alert('無法搜尋到相關書籍');
-    }else{
+function searchRender(r, data) {
+  location.search = `result=${r}`;
+  let newData = data.filter((item) => item.bookName.includes(r));
+  if (newData.length === 0) {
+    alert("無法搜尋到相關書籍");
+  } else {
     searchPageRender(newData);
     searchPageSelectCategory.options[0].selected = true;
     searchPageSelectStar.options[0].selected = true;
   }
 }
 
-function searchRenderfromPage(r,data){
-    
-  let newData  = data.filter((item) => item.bookName.includes(r));
+function searchRenderfromPage(r, data) {
+  let newData = data.filter((item) => item.bookName.includes(r));
   searchPageRender(newData);
   searchPageSelectCategory.options[0].selected = true;
   searchPageSelectStar.options[0].selected = true;
-
 }
-
-
 
 //select render
 
- function selectRender(e){
+function selectRender(e) {
   const CateValue = searchPageSelectCategory.value;
-  const StarValue = searchPageSelectStar.value
+  const StarValue = searchPageSelectStar.value;
 
-           if(e.target.value === undefined){
-               return;
-
-           }else if(CateValue === '' && StarValue === ''){
-               searchPageRender(data);
-
-           }else if(CateValue !== '' && StarValue === ''){  
-               searchPageSearch.value = ''; 
-               let newDataFormCategory = data.filter((item) => {
-                   return  item.tags.includes(CateValue) ;
-                     });
-                   searchPageRender(newDataFormCategory);
-
-           }else if(StarValue !== '' &&CateValue  === ''){
-               searchPageSearch.value = ''; 
-               let newDataFromStar = data.filter((item) => {
-                   let newStar = parseInt(item.Star);
-                   let newStartoString = newStar.toString();
-                  return newStartoString.includes(StarValue)});
-                  searchPageRender(newDataFromStar);
-               
-           }else{
-                searchPageSearch.value = ''; 
-                let newDataFormCategory = data.filter((item) => {
-                    return  item.tags.includes(CateValue) ;
-                      });
-                      let newDataFromStar = newDataFormCategory.filter((item) => {
-                        let newStar = parseInt(item.Star);
-                        let newStartoString = newStar.toString();
-                       return newStartoString.includes(StarValue)});
-                       searchPageRender(newDataFromStar);
-           }
+  if (e.target.value === undefined) {
+    return;
+  } else if (CateValue === "" && StarValue === "") {
+    searchPageRender(data);
+  } else if (CateValue !== "" && StarValue === "") {
+    searchPageSearch.value = "";
+    let newDataFormCategory = data.filter((item) => {
+      return item.tags.includes(CateValue);
+    });
+    searchPageRender(newDataFormCategory);
+  } else if (StarValue !== "" && CateValue === "") {
+    searchPageSearch.value = "";
+    let newDataFromStar = data.filter((item) => {
+      let newStar = parseInt(item.Star);
+      let newStartoString = newStar.toString();
+      return newStartoString.includes(StarValue);
+    });
+    searchPageRender(newDataFromStar);
+  } else {
+    searchPageSearch.value = "";
+    let newDataFormCategory = data.filter((item) => {
+      return item.tags.includes(CateValue);
+    });
+    let newDataFromStar = newDataFormCategory.filter((item) => {
+      let newStar = parseInt(item.Star);
+      let newStartoString = newStar.toString();
+      return newStartoString.includes(StarValue);
+    });
+    searchPageRender(newDataFromStar);
+  }
 }
-
-
 
 //接收SearchResult from pages
 
-
 let value = window.location.search;
-const newvalue = decodeURI(value.split('=')[1]);
+const newvalue = decodeURI(value.split("=")[1]);
 
 //帶入SearchResult
 
-function searchResult(data){
-  if(newvalue === 'undefined'){
-     return;
-   }else if(newvalue === ''){
-    searchPageRender(data)
-   }else{
+function searchResult(data) {
+  if (newvalue === "undefined") {
+    return;
+  } else if (newvalue === "") {
+    searchPageRender(data);
+  } else {
     searchPageSearch.value = newvalue;
-    searchRenderfromPage(newvalue,data);
-   }
-
+    searchRenderfromPage(newvalue, data);
+  }
 }
 
 //move to pages.html
 
-const toPages = document.querySelector('.bookList');
-toPages.addEventListener('click', getBookId);
+const toPages = document.querySelector(".bookList");
+toPages.addEventListener("click", getBookId);
 
-
-function getBookId(e){
-    if(e.target.dataset.id === undefined){
-        return;   
-    }else{
-        let pageId = e.target.dataset.id;
-        console.log(e.target.dataset.id);
-        window.open(`http://127.0.0.1:5501/app/pages.html?Id=${pageId}`);
-    }
+function getBookId(e) {
+  if (e.target.dataset.id === undefined) {
+    return;
+  } else {
+    let pageId = e.target.dataset.id;
+    console.log(e.target.dataset.id);
+    window.open(
+      `https://ocket609.github.io/20_novel_search/app/pages.html?Id=${pageId}`
+    );
+  }
 }
-
-
-
 
 //判斷是否收藏書籍
 
 let goodBook = [];
 
-let bookLocal = localStorage.getItem('bookId');
+let bookLocal = localStorage.getItem("bookId");
 
-function GoodBookData(id,index){
+function GoodBookData(id, index) {
   let bookLocalData = JSON.parse(bookLocal);
-  if( bookLocalData === null){
-    localStorage.setItem('BookId',JSON.stringify([]));
+  if (bookLocalData === null) {
+    localStorage.setItem("BookId", JSON.stringify([]));
     return;
-  }else if( bookLocalData.indexOf(id) !== -1){
-    
+  } else if (bookLocalData.indexOf(id) !== -1) {
     goodBook.push(index);
-
- }
+  }
 }
 
 //渲染書籍收藏
 
-function GoodBookCheckForDisplay(){
-  
-  const bookHeart = document.querySelectorAll('.bookHeart'); 
-    goodBook.forEach((item) => {
-       
-      bookHeart[item].setAttribute('src', './assets/images/heart-full.svg')
-  })
+function GoodBookCheckForDisplay() {
+  const bookHeart = document.querySelectorAll(".bookHeart");
+  goodBook.forEach((item) => {
+    bookHeart[item].setAttribute("src", "./assets/images/heart-full.svg");
+  });
   goodBook = [];
 }
 
-
 //聆聽收藏
 
-const bookList = document.querySelector('.bookList');
+const bookList = document.querySelector(".bookList");
 
-bookList.addEventListener('click',GoodBooklistener);
+bookList.addEventListener("click", GoodBooklistener);
 
-function GoodBooklistener(e){
-
+function GoodBooklistener(e) {
   let bookLocalData = JSON.parse(bookLocal);
   let value = e.target.dataset.bookheartid;
   let numValue = Number(value);
 
-  if(value === null || value === NaN || value === undefined){
-    
+  if (value === null || value === NaN || value === undefined) {
     return;
-  }else{
-    if(bookLocalData.includes(numValue)){
+  } else {
+    if (bookLocalData.includes(numValue)) {
       console.log(1);
-      let index =  bookLocalData.indexOf(numValue);
+      let index = bookLocalData.indexOf(numValue);
       console.log(index);
-      bookLocalData.splice(index,1);
+      bookLocalData.splice(index, 1);
       console.log(bookLocalData);
-      localStorage.setItem('bookId',JSON.stringify(bookLocalData));
+      localStorage.setItem("bookId", JSON.stringify(bookLocalData));
       window.location.reload();
-  
-    }else{
+    } else {
       console.log(1);
       bookLocalData.push(numValue);
-      localStorage.setItem('bookId',JSON.stringify(bookLocalData));
+      localStorage.setItem("bookId", JSON.stringify(bookLocalData));
       window.location.reload();
     }
   }
 }
-
